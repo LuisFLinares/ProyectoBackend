ca0fb8a41cde6ff781e1aabbcab2bab8a8f3eb84
LuisFLinares <100984560+LuisFLinares@users.noreply.github.com>
2022-08-22 04:32:25 +0100

    Deploy

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -37,4 +39,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('authentication/', include('authentication.urls')),
-]
+]   
+
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+appname='mainapp'
